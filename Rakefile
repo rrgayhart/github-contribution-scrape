3711d98fd6f9b88a930bdeae93492e903d82fadc
Meeka <rrgayhart@gmail.com>
2014-01-10 17:31:11 -0700

    simplified db connection

--- Rakefile
+++ Rakefile
@@ -2,9 +2,6 @@ require "sinatra/activerecord/rake"
 require 'rake/testtask'
 require "./app"
 
-task :default => :test
-env = ENV["RACK_ENV"] || "development"
-
 Rake::TestTask.new do |t|
   t.libs << 'test'
   t.test_files = FileList["test/**/*_test.rb"]
--- app.rb
+++ app.rb
@@ -1,19 +1,20 @@
 require 'sinatra' 
 require 'sinatra/activerecord'
+require './config/environments'
 require 'json'
 Dir[File.dirname(__FILE__) + '/models/*.rb'].each {|file| require file }
 
-env_index = ARGV.index("-e")
-env_arg = ARGV[env_index + 1] if env_index
-env = env_arg || ENV["SINATRA_ENV"] || "development"
+# env_index = ARGV.index("-e")
+# env_arg = ARGV[env_index + 1] if env_index
+# env = env_arg || ENV["SINATRA_ENV"] || "development"
 
-use ActiveRecord::ConnectionAdapters::ConnectionManagement # close connection to the DDBB properly...https://github.com/puma/puma/issues/59
-databases = YAML.load_file("config/database.yml")
-ActiveRecord::Base.establish_connection(databases[env])
+# use ActiveRecord::ConnectionAdapters::ConnectionManagement # close connection to the DDBB properly...https://github.com/puma/puma/issues/59
+# databases = YAML.load_file("config/database.yml")
+# ActiveRecord::Base.establish_connection(databases[env])
 
-if env == 'test'
-  User.destroy_all
-end
+# if env == 'test'
+#   User.destroy_all
+# end
 
 
 get '/' do
